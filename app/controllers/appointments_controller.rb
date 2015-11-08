fd8ba1642b771980361dbdbf85f96f5d4f1936c9
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-08 11:00:23 -0500

    Add appointment association to User model

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -63,12 +63,13 @@ class AppointmentsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
     def set_appointment
       @appointment = Appointment.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
-      params.require(:appointment).permit(:event, :location, :start_time, :end_time)
+      params.require(:appointment).permit(:event, :location, :start_time, :end_time, :user_id)
     end
 end
